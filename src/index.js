bc2f169e9556ddda34985d0e002c1b7741a99d21
Sanyu <sa.los@mail.ru>
2018-09-30 18:09:53 +0300

    Easy,kak i warmup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+	let i = 0;
+ for(;number >=5 ;)
+ {
+ i += Math.trunc( number/5 );
+ number = Math.trunc( number/5 );
+ 
+}
+return i;
 }
